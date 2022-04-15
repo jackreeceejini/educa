77c8bc8306e6842ee5fd60473e0468562a4b3a62
jackreece Ejini <jackreeceejini@gmail.com>
2022-04-15 14:45:41 -0400

    added content upload and viewing

--- educa/settings.py
+++ educa/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy 
 
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list') 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'students.apps.StudentsConfig',
+    'embed_video', 
 ]
 
 MIDDLEWARE = [
@@ -120,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') 
