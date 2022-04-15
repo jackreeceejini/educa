8c73beda67d9c5e38c4658e19e6bfcff34b37083
jackreece Ejini <jackreeceejini@gmail.com>
2022-04-15 10:02:12 -0400

    added course listing with detail

--- courses/views.py
+++ courses/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView 
+from django.views.generic.detail import DetailView 
 from .models import Course 
 from django.urls import reverse_lazy 
 from django.views.generic.list import ListView 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView 
 from django.views.generic.base import TemplateResponseMixin, View 
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject 
 from .forms import ModuleFormSet
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin 
 from django.forms.models import modelform_factory
 from django.apps import apps
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.db.models import Count 
 
 
 
@@ -147,3 +149,22 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
 
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course 
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects, 
+        'subject': subject, 'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course 
+    template_name = 'courses/course/detail.html'
