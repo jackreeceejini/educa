ba4952ff593238613aca2f91edf668b8d7429189
jackreece Ejini <jackreeceejini@gmail.com>
2022-04-16 13:41:03 -0400

    added chat app

---
+++ chat/urls.py
@@ -0,0 +1,11 @@
+import django
+
+
+from django.urls import path
+from . import views 
+
+app_name = 'chat' 
+
+urlpatterns = [
+    path('room/<int:course_id>/', views.course_chat_room, name='course_chat_room'),
+]
